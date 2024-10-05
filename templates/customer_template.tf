6abf0a57bca650552f8586cd0287b3731b1e3b31
nmadesh20 <47688833+nmadesh20@users.noreply.github.com>
2024-10-05 11:34:11 +0530

    Update customer_template.tf

--- templates/customer_template.tf
+++ templates/customer_template.tf
@@ -7,11 +7,11 @@ module "test" {
             repo_extension = "0001-0004"
             projects = {
                 "0108" = {
-                    number_of_projects = <<number_of_projects>>
-                    project_extension = <<project_extension>>
-                    group = <<group>>
-                    au = <<au>>
-                    ticket = <<ticket>>
+                    number_of_projects = "<+stage.variables.number_of_projects>"
+                    project_extension = "<stage.variables.project_extension>"
+                    group = "<stage.variables.group>"
+                    au = "<stage.variables.au>"
+                    ticket = "<stage.variables.ticket>"
                 }
             }
         }
